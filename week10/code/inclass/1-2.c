a5ee365b1bce210ec86135532dbfb7c9ef443b8e
zjh <1162812451@qq.com>
2019-11-12 16:37:35 -0800

    week10 class1

---
+++ week10/code/inclass/1-2.c
@@ -0,0 +1,32 @@
+#include "my.h"
+
+int g=10;
+
+int main()
+{
+  int t=20;
+  static int s=30;
+  pid_t pid;
+  pid=vfork();
+  if(pid<0)
+  {
+    perror("vfork failed!\n");
+    exit(-1);
+  }
+  else if(pid==0)
+  {
+    printf("before child pid=%d:&g=%p,&t=%p,&s=%p\n", getpid(),&g,&t,&s);
+    g=100;
+    t=200;
+    s=300;
+    printf("after child pid=%d:g=%d,t=%d,s=%d\n",getpid(),g,t,s);
+    sleep(3);
+    _exit(0);
+  }
+  else
+  {
+    printf("parent pid=%d:&g=%p,&t=%p,&s=%p\n", getpid(),&g,&t,&s);
+    printf("parent pid=%d:g=%d,t=%d,s=%d\n",getpid(),g,t,s);
+    return 0;
+  }
+}
