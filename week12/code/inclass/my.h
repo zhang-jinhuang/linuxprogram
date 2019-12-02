72bcc9b284e06a209c5e9218e512ec1972746466
zjh <1162812451@qq.com>
2019-12-02 04:51:31 -0800

    week 12

--- week12/code/inclass/my.h
+++ week12/code/inclass/my.h
@@ -4,3 +4,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <errno.h>
+#include <wait.h>
+#include <time.h>
+#include <signal.h>
+#include <fcntl.h>
+
+void sighandle(int);
