b78382336c35b935fb981ccbb0c8ce9588e2d736
zjh <1162812451@qq.com>
2019-12-16 16:29:35 -0800

    week15 first commit

---
+++ week15/code/inclass/my.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<limits.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<pthread.h>
+
+#define NUM 4
+
+#define LOOP 10000000
